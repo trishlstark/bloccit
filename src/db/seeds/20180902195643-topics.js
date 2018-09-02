5e30a1edb34c5f2809eb4140c793d89580cbdd8c
Trisha Stark <trishlstark@gmail.com>
2018-09-02 14:00:55 -0600

    TopicResource-02-Checkpoint Complete

---
+++ src/db/seeds/20180902195643-topics.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const faker = require("faker");
+
+let topics = [];
+
+for(let i = 1 ; i <= 15 ; i++){
+  topics.push({
+    title: faker.hacker.noun(),
+    description: faker.hacker.phrase(),
+    createdAt: new Date(),
+    updatedAt: new Date()
+  });
+ }
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+     return queryInterface.bulkInsert("Topics", topics, {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete("Topics", null, {});
+  }
+};
