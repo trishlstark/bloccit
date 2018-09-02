15c1324f9e816e17bb54de564eadb665a9c02f26
Trisha Stark <trishlstark@gmail.com>
2018-09-02 10:17:56 -0600

    Routing Controllers and the view assignment complete

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -4,15 +4,23 @@ const base = "http://localhost:3000/";
 
 describe("routes : static", () => {
     describe("GET /", () => {
-      it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", () => {
+        it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", (done) => {
+            request.get(base, (err, res, body) => {
+                expect(res.statusCode).toBe(200);
+                expect(body).toContain("Welcome to Bloccit");
 
-      request.get(base, (err, res, body) => {
-        expect(res.statusCode).toBe(200);
-        expect(body).toContain("Welcome to Bloccit");
-
-        done();
-      });
+                done();
+            });
+        });
     });
 
+    describe("GET /about", () => {
+      it("should have 'About Us' in the body of the response", (done) => {
+          request.get(base, (err, res, body) => {
+              expect(body).toContain("About Us");
+
+              done();
+          });
+      });
   });
 });
--- src/routes/static.js
+++ src/routes/static.js
@@ -4,4 +4,7 @@ const staticController = require("../controllers/staticController");
 
 router.get("/", staticController.index);
 
+router.get("/about", staticController.about);
+
+
 module.exports = router;
