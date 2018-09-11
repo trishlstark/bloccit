5c3a45af51964b50b52535e61b44d13a9de734ee
Trisha Stark <trishlstark@gmail.com>
2018-09-11 09:02:41 -0600

    Checkpoint Post Resource 02 Complete

--- src/controllers/topicController.js
+++ src/controllers/topicController.js
@@ -33,6 +33,7 @@ module.exports = {
 
     show(req, res, next){
       topicQueries.getTopic(req.params.id, (err, topic) => {
+
         if(err || topic == null){
           res.redirect(404, "/");
         } else {
